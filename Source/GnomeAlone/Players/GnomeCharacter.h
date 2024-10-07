8102aad3b02238e18a9a4111519e7282f535611f
Quincy Michels <quincy.michels@gmail.com>
2024-10-07 10:17:47 -0400

    added exterminator and gnome cpp files

---
+++ Source/GnomeAlone/Players/GnomeCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GnomeAlone/GnomeAloneCharacter.h"
+#include "GnomeCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GNOMEALONE_API AGnomeCharacter : public AGnomeAloneCharacter
+{
+	GENERATED_BODY()
+	
+};
